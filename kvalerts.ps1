5350305b717fe6038fdf85721e3606ad03a19158
chamberslad <lee.d.chambers@gmail.com>
2020-02-20 17:41:58 +0000

    Create kvalerts.ps1

---
+++ kvalerts.ps1
@@ -0,0 +1,21 @@
+param
+(
+[Parameter (Mandatory = $false)]
+[object] $WebhookData
+)
+
+#If runbook was called from Webhook, WebhookData will not be null.
+if ($WebhookData) {
+
+#rotate secret:
+#generate new secret version in key vault
+#update db/service with generated secret
+
+#Write-Output "WebhookData <$WebhookData>"
+Write-Output $WebhookData.RequestBody
+}
+else
+{
+# Error
+write-Error "No input data found." 
+}
